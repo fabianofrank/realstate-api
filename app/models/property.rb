35abceeece2b0f4ee8b34d42659cbf19a5128724
fabianofrank <fabianofrank@protonmail.com>
2022-11-02 11:45:58 -0300

    Resize images with model method

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,4 +4,10 @@ class Property < ApplicationRecord
   has_many_attached :images
 
   validates :name, presence: true, length: { in: 3..75 }
+
+  def image_as_thumbnail
+    images.map do |image|
+      image.variant(resize_to_limit: [800, 800]).processed
+    end
+  end
 end
